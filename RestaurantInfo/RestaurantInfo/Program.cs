8268ce578d4c4e563f9de059710d47efb87d4175
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-11-19 18:52:38 +0200

    Refactoring

--- RestaurantInfo/RestaurantInfo/Program.cs
+++ RestaurantInfo/RestaurantInfo/Program.cs
@@ -19,7 +19,7 @@ namespace RestaurantInfo
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                   .UseStartup<Startup>()
+                   .Build();
     }
 }
