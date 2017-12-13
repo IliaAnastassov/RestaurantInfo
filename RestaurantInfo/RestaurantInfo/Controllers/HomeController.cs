33cea591fdaf22e2870481c481d7d0192233f970
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-12-13 16:29:30 +0200

    Add new features

--- RestaurantInfo/RestaurantInfo/Controllers/HomeController.cs
+++ RestaurantInfo/RestaurantInfo/Controllers/HomeController.cs
@@ -1,20 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RestaurantInfo.Services;
+using RestaurantInfo.ViewModels;
 
 namespace RestaurantInfo.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetGreeting();
+
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = _restaurantData.Get(id);
+
+            if (model == null)
+            {
+                RedirectToAction(nameof(Index));
+            }
 
             return View(model);
         }
---
+++ RestaurantInfo/RestaurantInfo/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using RestaurantInfo.Models;
+
+namespace RestaurantInfo.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public string CurrentMessage { get; set; }
+    }
+}
