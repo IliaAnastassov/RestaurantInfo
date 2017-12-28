ad1d8ac594ea5ae754f3ad9137e8dd27842737d7
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-12-28 16:40:55 +0200

    Add DbContext

---
+++ RestaurantInfo/RestaurantInfo/Data/RestaurantInfoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantInfo.Models;
+
+namespace RestaurantInfo.Data
+{
+    public class RestaurantInfoDbContext : DbContext
+    {
+        public RestaurantInfoDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
