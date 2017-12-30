598dc9c4de9ee7ad02a39c1409f012f44ae1fc16
Ilia Anastassov <ilia.anastassov@gmail.com>
2017-12-30 18:09:11 +0200

    Register ef services

--- RestaurantInfo/RestaurantInfo/Startup.cs
+++ RestaurantInfo/RestaurantInfo/Startup.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RestaurantInfo.Data;
 using RestaurantInfo.Services;
 
 namespace RestaurantInfo
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddDbContext<RestaurantInfoDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("RestaurantInfo")));
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddMvc();
         }
