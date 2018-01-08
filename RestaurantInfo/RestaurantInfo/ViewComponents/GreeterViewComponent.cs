986e4e96e947033de4e2478e88f9724932d54fd1
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-01-08 19:40:45 +0200

    Add Greeter ViewComponent with Default view

---
+++ RestaurantInfo/RestaurantInfo/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantInfo.Services;
+
+namespace RestaurantInfo.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var message = _greeter.GetGreeting();
+            return View("Default", message);
+        }
+    }
+}
